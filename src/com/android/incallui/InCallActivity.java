51911d1900341c15f9687b0b34675a9223f6cdec
Garik Badalyan <garikb@codeaurora.org>
2014-10-23 18:33:16 -0700

    IMS-VT: Enable SIP based video multitasking.
    Enable SIP based video multitasking.
    
    Change-Id: I3731e64617bde9278f13ff94ff829a3a90f467ce

--- src/com/android/incallui/InCallActivity.java
+++ src/com/android/incallui/InCallActivity.java
@@ -234,6 +234,7 @@ public class InCallActivity extends Activity {
         // It is possible that the activity restarted because orientation changed.
         // Notify listeners if orientation changed.
         doOrientationChanged(getResources().getConfiguration().orientation);
+        InCallPresenter.getInstance().onActivityStarted();
     }
 
     @Override
@@ -282,6 +283,7 @@ public class InCallActivity extends Activity {
         Log.d(this, "onStop()...");
 
         InCallPresenter.getInstance().updateIsChangingConfigurations();
+        InCallPresenter.getInstance().onActivityStopped();
         super.onStop();
     }
 
